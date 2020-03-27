4191498a698d18fa49f7561c2f11dad7fb99f0ae
System Administrator <jon.s.solari@gmail.com>
2020-03-27 17:22:38 -0400

    :sparkles: basic REPL begun
    basic REPL begun

--- game.py
+++ game.py
@@ -2,8 +2,23 @@
 
 # Player is an object, but only to have inventory & progression through the story
 
+class Player:
+    def __init__(self, prog, inv):
+        self.prog = 0
+        self.inv = []
+
 # Progression attribute will increment as you go on, and this will dictate what's shown as a prompt
 
-class Player:
-    def __init__(self, prog):
-        self.prog = 0
+class Section: 
+    def __init__(self, id, desc, opt):
+        self.id = id
+        self.desc = desc
+        self.opt = []
+
+while True:
+    print("Welcome! Press Q to quit.")
+
+    cmd = input("--->")
+
+    if cmd == "q":
+        break
