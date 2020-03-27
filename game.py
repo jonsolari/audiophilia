f0a1af94a4a67a4075a3cecfe713f4148ea52e5f
System Administrator <jon.s.solari@gmail.com>
2020-03-27 17:15:15 -0400

    :sparkles: basic structure document
    basic structure document

---
+++ game.py
@@ -0,0 +1,6 @@
+# STRUCTURE
+
+# Player is an object, but only to have inventory & progression through the story
+
+# Progression attribute will increment as you go on, and this will dictate what's shown as a prompt
+
