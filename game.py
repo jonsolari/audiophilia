da950d1e5ebe5a9b737aede5b3b08b265aea3ded
System Administrator <jon.s.solari@gmail.com>
2020-03-27 17:56:51 -0400

    :construction: fstring not cooperating
    fstring not cooperating

--- game.py
+++ game.py
@@ -3,9 +3,12 @@
 # Player is an object, but only to have inventory & progression through the story
 
 class Player:
-    def __init__(self, prog, inv):
+    def __init__(self, name, prog, inv):
+        self.name = name
         self.prog = 0
         self.inv = []
+
+player = Player("", 0, [])
 
 # Progression attribute will increment as you go on, and this will dictate what's shown as a prompt
 
@@ -15,10 +18,35 @@ class Section:
         self.desc = desc
         self.opt = []
 
+parts = {
+    0: Section(0, "Please enter your name (or Q to quit).", []),
+    1: Section(1, f"Hello, {player.name}!", [])
+}
+
+
+
 while True:
-    print("Welcome! Press Q to quit.")
+    current = player.prog
+    
+    print(parts[current].desc)
 
-    cmd = input("--->")
+    cmd = input("-->")
 
-    if cmd == "q":
-        break
+    if current == 0:
+        if cmd == "q" or cmd == "Q":
+            break
+        else: 
+            player.name = cmd
+            # print(player.name)
+            player.prog = 1
+            # print(player.prog)
+    elif current == 1:
+        if cmd == "q" or cmd == "Q":
+            break
+
+
+    # if cmd == "q" or cmd == "Q":
+    #     break
+    # else: 
+    #     print("I didn't understand that.")
+    #     print()
